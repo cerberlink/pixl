3d1794b429551f98f44aff72b3e990ba605d243d
Carlos <guerreroc47@gmail.com>
2020-05-05 14:10:35 -0700

    formatted

--- myapp/app.js
+++ myapp/app.js
@@ -15,7 +15,9 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "views")));
