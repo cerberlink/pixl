a68c4d3446b59484d42c2a4123ad08742f86d9a7
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-05-05 01:09:53 -0700

    add routers for elements

--- myapp/routes/al.js
+++ myapp/routes/al.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var app = express();
+var path = require("path");
 var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-    res.send("respond with a resource");
+  res.sendFile(path.join(__dirname + "/elements.html"));
 });
 
 module.exports = router;
